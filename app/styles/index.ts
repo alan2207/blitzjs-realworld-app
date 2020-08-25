d6c5c7fcc88a2eb698d26868b92ee027305e5817
Alan <alan2207@live.com>
2020-08-25 12:32:02 +0200

    minor updates

--- app/styles/index.ts
+++ app/styles/index.ts
@@ -3,5 +3,5 @@ export const cardStyles = (colorMode = "light") => ({
   shadow: "md",
   bg: colorMode === "light" ? "white" : "gray.800",
   p: "8",
-  border: "1px solid lightgray",
+  border: colorMode === "light" ? "none" : "1px solid grey",
 })
